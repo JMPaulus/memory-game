2bf7872e22bcc91880273f85b89b307c1b753a17
JMPAULUS <jmtpaulus@gmail.com>
2019-01-30 19:01:48 +0100

    still can't find how to display the dino picture

--- memory.js
+++ memory.js
@@ -1,35 +1,36 @@
 var cards = ["apatosaurus.png","apatosaurus.png", "tyranosaurus.png", "velociraptor.png", "diplodocus.png", "triceratops.png", 
 "brachiosaurus.png", "tyranosaurus.png", "velociraptor.png", "diplodocus.png", "triceratops.png", "brachiosaurus.png"];
+// can't find this bad dino to open card
 
-var card0 = document.getElementById("card0"); // 1. add element from HTML and give the ID to it
-var card1 = document.getElementById("card1");
-var card2 = document.getElementById("card2");
-var card3 = document.getElementById("card3");
+var k0 = document.getElementById('k0'); // 1. add element from HTML and give the ID to it
+var k1 = document.getElementById('k1');
+var k2 = document.getElementById('k2');
+var k3 = document.getElementById('k3');
 
-var card4 = document.getElementById("card4");
-var card5 = document.getElementById("card5");
-var card6 = document.getElementById("card6");
-var card7 = document.getElementById("card7");
+var k4 = document.getElementById('k4');
+var k5 = document.getElementById('k5');
+var k6 = document.getElementById('k6');
+var k7 = document.getElementById('k7');
 
-var card8 = document.getElementById("card8");
-var card9 = document.getElementById("card9");
-var card10 = document.getElementById("card10");
-var card11 = document.getElementById("card11");
+var k8 = document.getElementById('k8');
+var k9 = document.getElementById('k9');
+var k10 = document.getElementById('k10');
+var k11 = document.getElementById('k11');
 
-card0.addEventListener("click", function() {openCard(0);}); // 2. listening to the click event next --> function
-card1.addEventListener("click", function() {openCard(1);});
-card2.addEventListener("click", function() {openCard(2);});
-card3.addEventListener("click", function() {openCard(3);});
+k0.addEventListener("click", function() {openCard(0);}); // 2. listening to the click event next --> function
+k1.addEventListener("click", function() {openCard(1);});
+k2.addEventListener("click", function() {openCard(2);});
+k3.addEventListener("click", function() {openCard(3);});
 
-card4.addEventListener("click", function() {openCard(4);});
-card5.addEventListener("click", function() {openCard(5);});
-card6.addEventListener("click", function() {openCard(6);});
-card7.addEventListener("click", function() {openCard(7);});
+k4.addEventListener("click", function() {openCard(4);});
+k5.addEventListener("click", function() {openCard(5);});
+k6.addEventListener("click", function() {openCard(6);});
+k7.addEventListener("click", function() {openCard(7);});
 
-card8.addEventListener("click", function() {openCard(8);});
-card9.addEventListener("click", function() {openCard(9);});
-card10.addEventListener("click", function() {openCard(10);});
-card11.addEventListener("click", function() {openCard(11);});
+k8.addEventListener("click", function() {openCard(8);});
+k9.addEventListener("click", function() {openCard(9);});
+k10.addEventListener("click", function() {openCard(10);});
+k11.addEventListener("click", function() {openCard(11);});
 
 var firstCard = false; // 4. boolean for first card
 var numberOfMoves = 0; // 5. turn counter
@@ -39,9 +40,9 @@ function openCard(nr) // 3. the core of the game is here
 {  // alert(nr);
     var picture = "url(Pictures/" + cards[nr] + ")";
 
-    $("#card" + nr).css("background-picture", picture); //9. jQuery adds class, here we add class to the card from css and then picture
-    $("#card" + nr).addClass("cardFirst"); // 10.we are adding class when clicking card the name of class is in second quote
-    $("#card" + nr).removeClass("card"); // 11. removing class after revealing second card
+    $('#k' + nr).css("background-image", picture); //9. jQuery adds class, here we add class to the card from css and then picture
+    $('#k' + nr).addClass("cardFirst"); // 10.we are adding class when clicking card the name of class is in second quote
+    $('#k' + nr).removeClass("card"); // 11. removing class after revealing second card
 
     if (firstCard== false)// 12.opening first card
     {
